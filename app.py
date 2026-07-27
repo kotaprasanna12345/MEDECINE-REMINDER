c72dfdc5a89dbf397d270fecdb58b1cc7fae6a4d
kota Sai prasanna <kotaprasanna3055@gmail.com>
2026-07-27 23:07:23 +0530

    Reorganize Flask app structure and routes

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template, request, jsonify
 import os
 
-app = Flask(__name__)
-
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
 @app.route('/')
 def home():
     return render_template('index.html')
